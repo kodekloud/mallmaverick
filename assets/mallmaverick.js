fedf355354052dad096977bf39ee7797ee9bba57
andy@mobilefringe.com <andy@mobilefringe.com>
2015-03-19 23:53:48 -0400

    via kodekloud.io (54205f856a616d39c5c80000)

--- assets/mallmaverick.js
+++ assets/mallmaverick.js
@@ -197,7 +197,7 @@ function hasImage(image_url){
 function getPostList(){
     initData();
     var mallDataJSON = JSON.parse(sessionStorage.mallData);
-    return mallDataJSON.posts;
+    return mallDataJSON.blogs;
 }
 function getPostDetailsBySlug(slug){
     initData();
