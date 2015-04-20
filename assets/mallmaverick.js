126e6cd58401ed04e8fdf10e9d9fc5bf58de93d0
peter@mobilefringe.com <peter@mobilefringe.com>
2015-04-20 16:55:21 -0400

    via kodekloud.io (54205f856a616d39c5c80000)

--- assets/mallmaverick.js
+++ assets/mallmaverick.js
@@ -237,7 +237,7 @@ function getPostsByKeyword(keyword){
     var posts = [];
 
     $.each( allPosts, function( index, value ) {
-        if(){
+        if(value.){
             posts.push(value);
             return false;
         }
