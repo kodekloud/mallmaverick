513e3ba9f955d65c184d1672539e65c5a54e6dcc
mark@mobilefringe.com <mark@mobilefringe.com>
2015-04-27 17:28:45 -0400

    via kodekloud.io (54205f856a616d39c5c80000)

--- assets/mallmaverick.js
+++ assets/mallmaverick.js
@@ -230,11 +230,21 @@ function hasImage(image_url){
     return true;
 }
 
-
-function getPostList(){
+function getBlogByName(){
     initData();
     var mallDataJSON = JSON.parse(sessionStorage.mallData);
     return mallDataJSON.blogs[0].posts;
+}
+
+function getPostList(name){
+    initData();
+    var folder = [];
+    var mallDataJSON = JSON.parse(sessionStorage.mallData);
+    $.each( mallDataJSON.blogs, function( index, value ) {
+        if(val.name == name){
+            return val;
+        }        
+    }
 }
 function getPostDetailsBySlug(slug){
     initData();
