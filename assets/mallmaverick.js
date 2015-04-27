694dd3faf84d898c2ddc09cbdfe17bf4c134f176
mark@mobilefringe.com <mark@mobilefringe.com>
2015-04-27 17:34:53 -0400

    via kodekloud.io (54205f856a616d39c5c80000)

--- assets/mallmaverick.js
+++ assets/mallmaverick.js
@@ -237,7 +237,7 @@ function getBlogByName(folderName){
     
     $.each( mallDataJSON.blogs, function( index,  value) {
         if(value.name == folderName){
-            console.log( value);
+            console.log(value);
             return value;
         }
         
