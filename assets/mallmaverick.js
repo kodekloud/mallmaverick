6a5c5b499105fa4696fd8294f91ef902c3fbbcde
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-16 10:39:19 -0400

    via kodekloud.io (54205f856a616d39c5c80000)

--- assets/mallmaverick.js
+++ assets/mallmaverick.js
@@ -439,7 +439,7 @@ function getNextPostBySlug(slug){
 }
 
 function getPrevFashionBySlug(slug){
-    var posts = getFashion();
+    var posts = getFashions();
     posts.sort(function(a, b){
         if(a.publish_date > b.publish_date) return -1;
         if(a.publish_date < b.publish_date) return 1;
@@ -456,7 +456,7 @@ function getPrevFashionBySlug(slug){
     return prevPost;
 }
 function getNextFashionBySlug(slug){
-    var posts = getFashion();
+    var posts = getFashions();
     posts.sort(function(b, a){
         if(a.publish_date > b.publish_date) return -1;
         if(a.publish_date < b.publish_date) return 1;
