64abf8622de4831d568d488a222272332ab30b5f
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-16 16:10:05 -0400

    via kodekloud.io (54205f856a616d39c5c80000)

--- assets/mallmaverick.js
+++ assets/mallmaverick.js
@@ -354,7 +354,7 @@ function getBlogByName(folderName){
     return folder[0].posts;
 }
 
-function getBlogByName(folderName){
+function getBlogInfoByName(folderName){
     
     initData();
     var mallDataJSON = JSON.parse(sessionStorage.mallData);
