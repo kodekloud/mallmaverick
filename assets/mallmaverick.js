e2cb5f1293d5a3e963e9c70d76cf44081569e7ff
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-17 13:59:38 -0400

    via kodekloud.io (54205f856a616d39c5c80000)

--- assets/mallmaverick.js
+++ assets/mallmaverick.js
@@ -399,6 +399,7 @@ function getPrevPublishedPostBySlug(slug){
 function getNextPublishedPostBySlug(slug){
     var post = getPublishedPostDetailsBySlug(slug);
     var posts = getAllPublishedPosts().sortBy(function(o){ return o.publish_date }).reverse();
+    console.log(post.slug);
     $.each(posts, function(i, val){
         console.log(val.slug);
         if (val.slug == post.slug){
