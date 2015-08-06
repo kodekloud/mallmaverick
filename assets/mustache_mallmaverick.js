1130d3fad269a8ac0a360cc31af690f9a4d541bc
andy@mobilefringe.com <andy@mobilefringe.com>
2015-08-06 12:41:33 -0400

    via kodekloud.io (54205f856a616d39c5c90000)

--- assets/mustache_mallmaverick.js
+++ assets/mustache_mallmaverick.js
@@ -142,10 +142,6 @@ function renderStoresWithPromotionsTemplate(template_id,html_id){
     var template_html = $(template_id).html();
     Mustache.parse(template_html);
     
-    // added to stop pulling stores with future promotions
-    
-    all_stores = getStoresList();
-    
     $.each( getStoresList() , function( key, val ) {
         if(val['promotions'].length > 0){
             localizeObject(val);
